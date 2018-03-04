faffee76d81731324b55266684ba0e8665138712
PC <PC@PC-PC>
2018-03-04 17:02:09 +0100

    commit n.1 (agregar la clase Moto)

---
+++ PracticaTema4/src/es/studium/Moto/Moto.java
@@ -0,0 +1,33 @@
+package es.studium.Moto;
+
+public class Moto
+{
+	String matricula;
+	int cilindrada;
+	public Moto() 
+	{
+		matricula = "";
+		cilindrada = 0;
+	}
+	public Moto(String m, int c) 
+	{
+		matricula = m;
+		cilindrada = c;
+	}
+	public String getMatricula()
+	{
+		return matricula;
+	}
+	public int getCilindrada () 
+	{
+		return cilindrada;
+	}
+	public void setMatricula (String m) 
+	{
+		matricula = m;
+	}
+	public void setCilindrada (int c) 
+	{
+		cilindrada = c;
+	}
+}
